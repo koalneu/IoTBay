da784f4761d92cf06602d567b1c3ab26cb0603c1
wilsonnh123 <126222338+wilsonnh123@users.noreply.github.com>
2023-05-12 23:43:21 +1000

    Add files via upload

---
+++ src/java/DAO/DBManager.java
@@ -0,0 +1,165 @@
+package DAO;
+import DAO.DBConnector;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import models.User;
+
+public class DBManager {
+    private Statement st;
+    private Connection conn;
+
+    public DBManager(Connection conn) throws SQLException{
+        st = conn.createStatement();
+        this.conn = conn;
+    }
+    
+    public User authenticateUser(String email, String password) {
+    try {
+        System.out.println("here1");
+        ResultSet resultSet = st.executeQuery("SELECT * FROM CUSTOMERS WHERE CUSTOMEREMAIL = '" + email + "'");
+        //Check if a email has not been found
+        if(!resultSet.next()){
+            return null;
+            
+        }
+        //Copy items from DB into a user object
+        User user = new User(
+            resultSet.getString("CUSTOMERFIRSTNAME"), 
+            resultSet.getString("CUSTOMERLASTNAME"), 
+            email, 
+            resultSet.getString("CUSTOMERPASSWORD"), 
+            resultSet.getString("CUSTOMERSTREET"),  
+            resultSet.getString("CUSTOMERPOSTCODE"),
+            resultSet.getString("CUSTOMERCITY"),
+            resultSet.getString("CUSTOMERSTATE"), 
+            resultSet.getString("CUSTOMERCOUNTRY"), 
+            resultSet.getString("USERTYPE")
+        );
+        //Check if the password matches the user's input
+        if(password.equals(user.getUserPassword())){
+            return user;
+        }
+        else{
+            return null;
+        }
+    } catch (SQLException ex) {
+        System.out.println("Error Establishing Connection!");
+        ex.printStackTrace();
+    }
+    return null;
+}
+
+    
+    public String userType(String email) {
+        try {
+            ResultSet resultSet = st.executeQuery("SELECT * FROM CUSTOMERS WHERE CUSTOMEREMAIL = '" + email + "'");
+            if (resultSet.next()) {
+                String userType = resultSet.getString("USERTYPE");
+                return userType;
+            }
+            
+        } catch (SQLException ex) {
+            System.out.println("Error Establishing Connection!");
+        }
+
+        return null;
+    }
+    
+    //Add user function
+    public void addUser(String fname, String lname, String email, String password, String street, String postcode, String city, String state, String country, String userType) throws SQLException, ClassNotFoundException{
+        try{
+            String command = "INSERT INTO CUSTOMERS(CUSTOMERID,CUSTOMERFIRSTNAME,CUSTOMERLASTNAME,CUSTOMEREMAIL,CUSTOMERPASSWORD,CUSTOMERSTREET,CUSTOMERPOSTCODE,CUSTOMERCITY,CUSTOMERSTATE,CUSTOMERCOUNTRY,USERTYPE) VALUES(?,?,?,?,?,?,?,?,?,?,?)";
+            PreparedStatement pst = conn.prepareStatement(command);
+            
+            //calculate the new ID
+            String rows = "select count(*) from CUSTOMERS";
+            ResultSet retrieveResult = st.executeQuery(rows);
+            retrieveResult.next();
+            int ID = retrieveResult.getInt(1);
+            
+            pst.setObject(1, ID);
+            pst.setObject(2, fname);
+            pst.setObject(3, lname);
+            pst.setObject(4, email);
+            pst.setObject(5, password);
+            pst.setObject(6, street);
+            pst.setObject(7, Integer.parseInt(postcode));
+            pst.setObject(8, city);
+            pst.setObject(9, state);
+            pst.setObject(10, country);
+            pst.setObject(11, userType);
+            pst.executeUpdate();
+        }
+        catch(Error e){
+        }
+            
+    }
+    
+    //update
+    public void updateUser(String originalEmail, String fname, String lname, String email, String password, String street, String postcode, String city, String state, String country) throws SQLException, ClassNotFoundException{
+        try {
+            //update is used to seelct
+            //Statement to update the correct columns
+            PreparedStatement update = conn.prepareStatement("UPDATE CUSTOMERS SET CUSTOMERFIRSTNAME=?, CUSTOMERLASTNAME=?, CUSTOMEREMAIL=?, CUSTOMERPASSWORD=?, CUSTOMERSTREET=?, CUSTOMERPOSTCODE=?, CUSTOMERCITY=?, CUSTOMERSTATE=?, CUSTOMERCOUNTRY=? WHERE CUSTOMEREMAIL=?");
+            //Set the variables for the "update" statement
+            update.setString(1, fname);
+            update.setString(2, lname);
+            update.setString(3, email);
+            update.setString(4, password);
+            update.setString(5, street);
+            update.setInt(6, Integer.parseInt(postcode));
+            update.setString(7, city);
+            update.setString(8, state);
+            update.setString(9, country);
+            update.setString(10, originalEmail);
+
+            update.executeUpdate();
+        }
+        catch(Error e){
+        }
+    }
+    
+    public void addAccessLogEntry(String email, String fname, String action) throws SQLException, ClassNotFoundException{
+        try{
+            String command = "INSERT INTO ACCESSLOGS(ACCESSLOGID,USERID,USERNAME,ACCESSTIME,ACTION) VALUES(?,?,?,?,?)";
+            PreparedStatement pst = conn.prepareStatement(command);
+
+            //calculate the ACCESSLOGID
+            String rows = "select count(*) from ACCESSLOGS";
+            ResultSet retrieveResult = st.executeQuery(rows);
+            retrieveResult.next();
+            int ACCESSLOGID = retrieveResult.getInt(1);
+
+            //retrieve userID from DB
+            String getUserID = "SELECT CUSTOMERID FROM CUSTOMERS WHERE CUSTOMEREMAIL = ?";
+            PreparedStatement userIDStatement = conn.prepareStatement(getUserID);
+            userIDStatement.setObject(1, email);
+            ResultSet retrieveID = userIDStatement.executeQuery();
+            retrieveID.next();
+            int userID = retrieveID.getInt(1);
+
+            //get timestamp
+            long timestamp = System.currentTimeMillis();
+            java.sql.Timestamp accessTime = new java.sql.Timestamp(timestamp);
+
+            pst.setObject(1, ACCESSLOGID);
+            pst.setObject(2, userID);
+            pst.setObject(3, fname);
+            pst.setObject(4, accessTime);
+            pst.setObject(5, action);
+
+            pst.executeUpdate();
+        }
+        catch(Error e){
+        }
+           
+    }
+    
+    
+}
---
+++ src/java/controllers/editController.java
@@ -0,0 +1,101 @@
+package controllers;
+
+import DAO.DBManager;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import models.User;
+
+/**
+ *
+ * @author Wilson
+ */
+public class editController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        Validator validator = new Validator();
+        User user = (User) session.getAttribute("user");
+        String originalEmail = user.getUserEmail();
+        validator.clear(session);
+        //retrieve data from input fields
+        
+        String fname = request.getParameter("fname");
+        String lname = request.getParameter("lname");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String street = request.getParameter("street");
+        String city = request.getParameter("city");
+        String postcode = request.getParameter("postcode");
+        String state = request.getParameter("state");
+        String country = request.getParameter("country");
+        
+        DBManager manager = (DBManager) session.getAttribute("manager");
+        //Validate inputs
+        if(!validator.validateStringPattern(fname)){
+            session.setAttribute("fnameErr", "Error: first name must be less than 50 characters");
+            System.out.println("fname");
+            request.getRequestDispatcher("edit.jsp").include(request, response);
+        }
+        else if(!validator.validateStringPattern(lname)){
+            session.setAttribute("lnameErr", "Error: last name must be less than 50 characters");
+            System.out.println("lnam");
+            request.getRequestDispatcher("edit.jsp").include(request, response);
+        }
+        else if(!validator.validateEmail(email)){
+            session.setAttribute("emailErr", "Error: email format incorrect");
+            System.out.println("em");
+            request.getRequestDispatcher("edit.jsp").include(request, response);
+        }
+        else if(!validator.validatePassword(password)){
+            session.setAttribute("passwordErr", "Error: password must be at least 8 characters");
+            System.out.println("pas");
+            request.getRequestDispatcher("edit.jsp").include(request, response);
+        }
+        else if(!validator.validateStringPattern(street)){
+            session.setAttribute("streetErr", "Error: street name must be less than 50 characters");
+            System.out.println("st");
+            request.getRequestDispatcher("edit.jsp").include(request, response);
+        }
+        else if(!validator.validateStringPattern(city)){
+            session.setAttribute("cityErr", "Error: city name must be less than 50 characters");
+            System.out.println("c");
+            request.getRequestDispatcher("edit.jsp").include(request, response);
+        }
+        else if(!validator.validatepostCode(postcode)){
+            session.setAttribute("postcodeErr", "Error: postcode format incorrect");
+            System.out.println("pc");
+            request.getRequestDispatcher("edit.jsp").include(request, response);
+        }
+        else if(!validator.validateStringPattern(country)){
+            session.setAttribute("countryErr", "Error: country name must be less than 50 characters");
+            System.out.println("c");
+            request.getRequestDispatcher("edit.jsp").include(request, response);
+        }
+        else{
+            try{
+                manager.updateUser(originalEmail,fname,lname,email,password,street,postcode,city,state,country);
+                manager.addAccessLogEntry(email, fname, "Update Details");
+                user = new User(fname, lname, email, password, street, postcode,city, state, country, null);
+                session.setAttribute("user", user);
+                request.getRequestDispatcher("profile.jsp").include(request, response);
+            }
+            catch (SQLException ex) {
+                Logger.getLogger(editController.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(editController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+        }
+    }
+
+   
+}
