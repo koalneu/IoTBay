11bd24a5c837fc10564dd1a9f497d60f265a0390
wilsonnh123 <126222338+wilsonnh123@users.noreply.github.com>
2023-05-15 20:44:29 +1000

    Add files via upload

---
+++ src/java/controllers/accesslogController.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controllers;
+
+import DAO.DBManager;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import models.User;
+
+/**
+ *
+ * @author Wilson
+ */
+public class accesslogController extends HttpServlet {
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("ACCESSLOG CONTROLLER");
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+        String email = user.getUserEmail();
+        DBManager manager = (DBManager) session.getAttribute("manager");
+        ArrayList<ArrayList<String>> accessLogs = manager.getAccessLogs(email);
+        request.setAttribute("accessLogs", accessLogs);
+        request.getRequestDispatcher("accesslog.jsp").forward(request, response);
+    }
+
+}
