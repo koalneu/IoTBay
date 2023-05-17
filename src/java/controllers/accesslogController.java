3ca97b6d663738e1099355bba81e5c1bdd24a614
ben-p3r3z <benjamin.perez@student.uts.edu.au>
2023-05-18 00:23:34 +1000

    running and up to date with main

--- src/java/controllers/accesslogController.java
+++ src/java/controllers/accesslogController.java
@@ -1,6 +1,6 @@
 package controllers;
 
-import DAO.DBManager;
+import models.dao.DBManager;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
--- src/java/models/dao/DBManager.java
+++ src/java/models/dao/DBManager.java
@@ -3,6 +3,8 @@ package models.dao;
 import models.Product;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import models.User;
 
 /**
@@ -122,6 +124,48 @@ public class DBManager {
         }
         catch(Error e){
         }
+    }
+    
+    public ArrayList<ArrayList<String>> getAccessLogs(String email){
+        try{
+            // Retrieve the ID from the Email
+            String fetch = "SELECT * FROM CUSTOMER WHERE CUSTOMEREMAIL = ?";
+            PreparedStatement stmt = conn.prepareStatement(fetch);
+            stmt.setString(1, email);
+            ResultSet rs = stmt.executeQuery();
+            rs.next();
+            String ID = rs.getString("CUSTOMERID");
+            // END OF THE FUNCTION
+            ArrayList<ArrayList<String>> accessLogs = new ArrayList<>();
+           
+            String query = "SELECT * FROM ACCESSLOGS WHERE USERID = ?";
+            PreparedStatement statement = conn.prepareStatement(query);
+            statement.setInt(1, Integer.parseInt(ID));
+            ResultSet resultSet = statement.executeQuery();
+                
+            while(resultSet.next()){
+                ArrayList<String> logEntry = new ArrayList<>();
+                
+                int accessLogID = resultSet.getInt("ACCESSLOGID");
+                String userName = resultSet.getString("USERNAME");
+                String accessTime = resultSet.getString("ACCESSTIME");
+                String action = resultSet.getString("ACTION");
+                
+                logEntry.add(accessLogID+"");
+                logEntry.add(ID+"");
+                logEntry.add(userName);
+                logEntry.add(accessTime);
+                logEntry.add(action);
+                
+                accessLogs.add(logEntry);
+            }
+            
+            return accessLogs;
+        }
+        catch (SQLException ex) {
+            Logger.getLogger(DBManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null; 
     }
     
     public void addAccessLogEntry(String email, String fname, String action) throws SQLException, ClassNotFoundException{
